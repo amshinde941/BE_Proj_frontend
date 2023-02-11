69f2d9bbbeddcb40999bf07d0b9edd2cfe4b46ce
Nikhil Dethe <dethenikhil7578@gmail.com>
2023-02-12 04:02:45 +0530

    HR Retention Done

--- src/Components/Hr/Retention.jsx
+++ src/Components/Hr/Retention.jsx
@@ -1,11 +1,97 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Checkbox } from "../CheckBox";
+import { RetentionData } from "../../Models/RetentionData";
+import { Button } from "../Button/Button";
 
 const Retention = () => {
-  return (
-    <div>
-      <h1>Retention ...</h1>
-    </div>
-  )
-}
+  const [checkboxes, setCheckboxes] = useState([
+    { id: 1, text: "Important / Non-Replacable Employees", checked: false },
+    { id: 2, text: "High Risk Employees", checked: false },
+    { id: 3, text: "Medium Risk Employees", checked: false },
+    { id: 4, text: "Low Risk Employees", checked: false },
+  ]);
 
-export default Retention
+  const handleChange = (id) => {
+    setCheckboxes(
+      checkboxes.map((checkbox) => {
+        if (checkbox.id === id) {
+          checkbox.checked = !checkbox.checked;
+        }
+        return checkbox;
+      })
+    );
+  };
+
+  return (
+    <>
+      <div className="m-10">
+        {/* Filters  */}
+        <h1 className="text-2xl font-bold">Filters</h1>
+        <hr className="my-2" />
+        <h2>Sort By</h2>
+        <div className="flex flex-wrap justify-between">
+          {checkboxes.map((checkbox) => (
+            <div key={checkbox.id} className="w-1/4 p-2">
+              <label className="block text-gray-700 font-medium">
+                <input type="checkbox" className="mr-2" />
+                {checkbox.text}
+              </label>
+            </div>
+          ))}
+        </div>
+
+        {/* Retention Information */}
+        <h1 className="text-2xl font-bold my-4">Retention Information</h1>
+        <hr className="my-2" />
+
+        <div className="mt-5 bg-slate-50 p-4 rounded-md flex flex-col gap-3 shadow hover:shadow-md cursor-pointer">
+          {RetentionData.map((retention) => {
+            return (
+              <>
+                
+                <div className="grid grid-cols-3">
+                  <div className="col-span-1">
+                    <h2 className="text-sky-800 mb-3 text-xl">{retention.name}</h2>
+                    <h6>Job Role: {retention.jobRole}</h6>
+                    <h3>Experience: {retention.experience}</h3>
+                    <h3>Location: {retention.location}</h3>
+                    <h3>Current Salary: {retention.cur_sal}</h3>
+                    <h3>Market Salary: {retention.market_sal}</h3>
+                    <h3>Current Leaves: {retention.cur_leaves}</h3>
+                    <h3>Manager Rating: {retention.rating}</h3>
+                  </div>
+                  <div class="col-span-1">
+                    <p className="text-sky-800 mb-3 text-xl"> Reasons To Leave Job </p>
+                    <h2>1.00 &lt;= Job Level &lt;= {retention.reasons.jobLevel}</h2>
+                    <h2>1.00 &lt;= Job Satisfaction &lt;= {retention.reasons.jobSat}</h2>
+                    <h2>1.00 &lt;= Role Satisfaction &lt;= {retention.reasons.curRole}</h2>
+                    <h2>1.00 &lt;= Salary Satisfaction &lt;= {retention.reasons.salary}</h2>
+                    <h2>1.00 &lt;= Location Satisfaction &lt;= {retention.reasons.location}</h2>
+                    <h2>1.00 &lt;= Timings Satisfaction &lt;= {retention.reasons.timings}</h2>
+                  </div>
+                  <div class="col-span-1">
+                    <p className="text-sky-800 mb-3 text-xl"> Retention Rate Of Particular Action </p>
+                    <div className="grid grid-cols-2">
+                    <span className="col-span-1 text-lg text-red-400">{retention.risk}</span>
+                      <span className="col-span-1">
+                        <Button>Take Action</Button>
+                      </span>
+                    </div>
+                    <h2>Job Satisfaction: {retention.actions.jobLevel}</h2>
+                    <h2>Increase in Role: {retention.actions.curRole}</h2>
+                    <h2>Increase in Salary: {retention.actions.salary}</h2>
+                    <h2>Change in Location: {retention.actions.location}</h2>
+                    <h2>Change in Timings: {retention.actions.timings}</h2>
+                  </div>
+                </div>
+                <hr className="my-2" />
+              </>
+            );
+          })}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Retention;
---
+++ src/Models/RetentionData.js
@@ -0,0 +1,264 @@
+export const RetentionData = [
+    
+        {
+            name: "Lynnell Bellow",
+            experience: 18,
+            jobRole: "Senior Developer",
+            location: "Eslöv",
+            cur_sal: "$232483.07",
+            market_sal: "$395346.87",
+            cur_leaves: 52,
+            rating: 3,
+            risk: "high",
+            reasons: {
+                jobLevel: 3.00,
+                jobSat: 2.04,
+                curRole: 3.20,
+                salary: 3.13,
+                location: 1.15,
+                timings: 1.00
+            },
+            actions: {
+                jobLevel: 60,
+                curRole: 68,
+                salary: 75,
+                location: 18,
+                timings: 13
+            }
+        },
+        {
+            name: "Kiah Godart",
+            experience: 2,
+            jobRole: "Junior Executive",
+            location: "Beraim",
+            cur_sal: "$85390.72",
+            market_sal: "$54134.43",
+            cur_leaves: 6,
+            rating: 10,
+            risk: "low",
+            reasons: {
+                jobLevel: 1.00,
+                jobSat: 1.04,
+                curRole: 2.20,
+                salary: 1.00,
+                location: 1.20,
+                timings: 1.00
+            },
+            actions: {
+                jobLevel: 10,
+                curRole: 11,
+                salary: 10,
+                location: 12,
+                timings: 10
+            }
+        },
+        {
+            name: "Ulrick Scurrell",
+            experience: 10,
+            jobRole: "Research Associate",
+            location: "Jandira",
+            cur_sal: "$108279.77",
+            market_sal: "$154220.53",
+            cur_leaves: 16,
+            rating: 6,
+            risk: "medium",
+            reasons: {
+                jobLevel: 2.00,
+                jobSat: 2.64,
+                curRole: 2.20,
+                salary: 3.13,
+                location: 2.00,
+                timings: 1.00
+            },
+            actions: {
+                jobLevel: 40,
+                curRole: 48,
+                salary: 75,
+                location: 18,
+                timings: 10
+            }
+        },
+        {
+            "name": "Marylin Dovidaitis",
+            "experience": 19,
+            "jobRole": "Geological Engineer",
+            "location": "Le Havre",
+            "cur_sal": "$337703.59",
+            "market_sal": "$367182.58",
+            "cur_leaves": 66,
+            "rating": 8,
+            "risk": "low",
+            "reasons": {
+                "jobLevel": 1.00,
+                "jobSat": 1.04,
+                "curRole": 2.20,
+                "salary": 1.00,
+                "location": 1.20,
+                "timings": 1.00
+            },
+            "actions": {
+                "jobLevel": 10,
+                "curRole": 11,
+                "salary": 10,
+                "location": 12,
+                "timings": 10
+            }
+        },
+        {
+            "name": "Ilysa Izzard",
+            "experience": 14,
+            "jobRole": "Executive Secretary",
+            "location": "Chenji",
+            "cur_sal": "$98944.14",
+            "market_sal": "$263742.61",
+            "cur_leaves": 9,
+            "rating": 2,
+            "risk": "high",
+            "reasons": {
+                "jobLevel": 3.00,
+                "jobSat": 2.04,
+                "curRole": 3.20,
+                "salary": 3.13,
+                "location": 1.15,
+                "timings": 1.00
+            },
+            "actions": {
+                "jobLevel": 60,
+                "curRole": 68,
+                "salary": 75,
+                "location": 18,
+                "timings": 13
+            }
+        },
+        {
+            "name": "Dix Muckersie",
+            "experience": 5,
+            "jobRole": "Senior Quality Engineer",
+            "location": "Wujia",
+            "cur_sal": "$346175.07",
+            "market_sal": "$138629.37",
+            "cur_leaves": 32,
+            "rating": 5,
+            "risk": "medium",
+            "reasons": {
+                "jobLevel": 2.00,
+                "jobSat": 2.64,
+                "curRole": 2.20,
+                "salary": 3.13,
+                "location": 2.00,
+                "timings": 1.00
+            },
+            "actions": {
+                "jobLevel": 40,
+                "curRole": 48,
+                "salary": 75,
+                "location": 18,
+                "timings": 10
+            }
+        },
+        {
+            "name": "Cecil Danet",
+            "experience": 14,
+            "jobRole": "Help Desk Operator",
+            "location": "Karolinka",
+            "cur_sal": "$196912.81",
+            "market_sal": "$129850.52",
+            "cur_leaves": 63,
+            "rating": 3,
+            "risk": "medium",
+            "reasons": {
+                "jobLevel": 2.00,
+                "jobSat": 2.64,
+                "curRole": 2.20,
+                "salary": 3.13,
+                "location": 2.00,
+                "timings": 1.00
+            },
+            "actions": {
+                "jobLevel": 40,
+                "curRole": 48,
+                "salary": 75,
+                "location": 18,
+                "timings": 10
+            }
+        },
+        {
+            "name": "Aubine Lipmann",
+            "experience": 13,
+            "jobRole": "Developer III",
+            "location": "Manquiring",
+            "cur_sal": "$142879.88",
+            "market_sal": "$73701.32",
+            "cur_leaves": 11,
+            "rating": 9,
+            "risk": "medium",
+            "reasons": {
+                "jobLevel": 2.00,
+                "jobSat": 2.64,
+                "curRole": 2.20,
+                "salary": 3.13,
+                "location": 2.00,
+                "timings": 1.00
+            },
+            "actions": {
+                "jobLevel": 40,
+                "curRole": 48,
+                "salary": 75,
+                "location": 18,
+                "timings": 10
+            }
+        },
+        {
+            "name": "Tabbie Vickar",
+            "experience": 15,
+            "jobRole": "Administrative Officer",
+            "location": "Odemira",
+            "cur_sal": "$268156.46",
+            "market_sal": "$405903.33",
+            "cur_leaves": 27,
+            "rating": 3,
+            "risk": "high",
+            "reasons": {
+                "jobLevel": 3.00,
+                "jobSat": 2.04,
+                "curRole": 3.20,
+                "salary": 3.13,
+                "location": 1.15,
+                "timings": 1.00
+            },
+            "actions": {
+                "jobLevel": 60,
+                "curRole": 68,
+                "salary": 75,
+                "location": 18,
+                "timings": 13
+            }
+        },
+        {
+            "name": "Tomasine Godbehere",
+            "experience": 19,
+            "jobRole": "Developer III",
+            "location": "Imrīsh",
+            "cur_sal": "$288032.12",
+            "market_sal": "$130029.86",
+            "cur_leaves": 35,
+            "rating": 10,
+            "risk": "low",
+            "reasons": {
+                "jobLevel": 1.00,
+                "jobSat": 1.04,
+                "curRole": 2.20,
+                "salary": 1.00,
+                "location": 1.20,
+                "timings": 1.00
+            },
+            "actions": {
+                "jobLevel": 10,
+                "curRole": 11,
+                "salary": 10,
+                "location": 12,
+                "timings": 10
+            }
+        }
+    
+]
