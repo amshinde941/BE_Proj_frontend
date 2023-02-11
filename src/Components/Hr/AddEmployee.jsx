691884ffb7a54ee5417eb057c5364e9468a53cb6
Nikhil Dethe <dethenikhil7578@gmail.com>
2023-02-12 04:12:26 +0530

    HR Add_Employee Done

--- src/Components/Hr/AddEmployee.jsx
+++ src/Components/Hr/AddEmployee.jsx
@@ -1,11 +1,31 @@
-import React from 'react'
+import React from "react";
+import { InputField } from "../../Components/InputField";
+import { PrimaryButton, SecondaryButton } from "../Button";
 
 const AddEmployee = () => {
   return (
-    <div>
-      <h1>Add Employees ....</h1>
-    </div>
-  )
-}
+    <>
+      <div className="m-10">
+        <div className=" flex gap-2 flex-col my-8">
+          <h1 className="text-2xl font-bold">Add Employee</h1>
+          <hr className="my-2" />
 
-export default AddEmployee
+          <InputField label="Name" placeholder="Employee Name" />
+          <InputField label="Email" placeholder="Employee Email" />
+          <InputField label="Employee Id" placeholder="Employee Id" />
+          <InputField label="Role" placeholder="Role" />
+          <InputField label="Salary" placeholder="Salary" />
+          <InputField
+            type="date"
+            label="Joining Date"
+            placeholder="Enter Date"
+          />
+
+          <PrimaryButton style={{ marginTop: "1rem" }}> Add </PrimaryButton>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default AddEmployee;
